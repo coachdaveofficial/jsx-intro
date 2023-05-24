538063068bb4de6eb792002ff5e0c3645c99f2d1
David Falson <david@falson.net>
2023-05-23 20:13:32 -0700

    reformat Person.js component

--- part-3/Person.js
+++ part-3/Person.js
@@ -2,10 +2,10 @@ const Person = ({name, age, hobbies}) => {
     return (
         <p className="person">
             Learn some information about this person.
-            {name.length > 6 ? <h2>Name: {name.substring(0,6)}</h2> : <h2>Name: {name}</h2>}
+            <h2>Name: {name.length > 6 ? name.substring(0,6) : name}</h2>
             Age: {age}
             <br></br>
-            {age >= 18 ? <h3>Please go vote!</h3> : <h3>You must be 18</h3>}
+            <h3>{age >= 18 ? 'Please go vote!' : 'You must be 18'}</h3>
             <br></br>
             <ul>
                 {hobbies.map((h) => <li>{h}</li>)}
